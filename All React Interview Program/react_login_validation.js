e00f346dd2c658cd266fcef96bb6e9bbc7531154
Sandeep Yagyash <guptasandeep188@gmail.com>
2018-12-27 17:33:29 +0530

    Update react_login_validation.js

--- All React Interview Program/react_login_validation.js
+++ All React Interview Program/react_login_validation.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
 
