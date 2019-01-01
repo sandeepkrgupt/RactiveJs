e0cf26359c389fcbfa5b4b94932d537a0df654a8
Sandeep Yagyash <guptasandeep188@gmail.com>
2019-01-02 00:24:44 +0530

    Delete Login.js

--- react project structure/project_name/components/digital-login-module/src/widgets/Login.js
+++
@@ -1,4 +0,0 @@
-import Component from 'digital-login-module/src/widget/Login.component';
-import Decorator from 'digital-login-module/src/widget/Login.decorator';
-
-export default Decorator(Component);
