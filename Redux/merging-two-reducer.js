1ab117c45cf0cceb5925a0b4c21add125e3b7163
Sandeep Yagyash <guptasandeep188@gmail.com>
2019-06-08 00:49:24 +0530

    Update merging-two-reducer.js

--- Redux/merging-two-reducer.js
+++ Redux/merging-two-reducer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { combineReducers, createStore } from 'redux';
 
-function productsReducer(state = [], action) {
+function productsReducer(state = [], args) {
     return state;
 }
 
-function userReducer(state = '', action) {
+function userReducer(state = '', args) {
     return state;
 }
 
@@ -35,7 +35,7 @@ console.log(store.getState());
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
