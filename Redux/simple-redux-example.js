0b29ff82b6f5055e7de5572a8ae656a36a0e249b
Sandeep Yagyash <guptasandeep188@gmail.com>
2019-06-08 00:49:56 +0530

    Update simple-redux-example.js

--- Redux/simple-redux-example.js
+++ Redux/simple-redux-example.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { createStore } from 'redux';
@@ -25,7 +25,7 @@ console.log(store.getState());
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
